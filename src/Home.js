ddef694f1149e9a182a40e6b1fd77bf2242b887f
Tony Maluki <tonymaluki@gmail.com>
2022-04-22 23:11:28 +0300

    learning how to create button events in react

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,21 @@
 const Home = () => {
   const handleClick = () => {
-      
+    alert("Clicked");
+  };
+  const handleClickAgain = (name) => {
+    alert("Hello " + name);
   };
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <button>Click me </button>
+      <button onClick={handleClick}>Click me </button>
+      <button
+        onClick={() => {
+          handleClickAgain("Mario");
+        }}
+      >
+        Click me Again
+      </button>
     </div>
   );
 };
