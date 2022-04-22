4606cae38c51adc254e161524728cd306e986f6e
Tony Maluki <tonymaluki@gmail.com>
2022-04-22 22:06:57 +0300

    learning how to nest react components

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Tony Blog</h1>
+      <div className="links">
+        <a href="/"></a>
+        <a href="/"></a>
+        <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
