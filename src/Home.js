5b198cbd71037d6e7094dba23c6eb466a029bcbc
Tony Maluki <tonymaluki@gmail.com>
2022-04-22 23:55:57 +0300

    learning useState hook and effects

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 const Home = () => {
+  //let name = "Tony";
+  const [name, setName] = useState("Tony");
+  const [age, setAge] = useState(25);
   const handleClick = () => {
-    alert("Clicked");
+    setName("Maluki");
+    setAge("30")
   };
   const handleClickAgain = (name) => {
     alert("Hello " + name);
@@ -8,14 +13,9 @@ const Home = () => {
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name} is {age} years old</p>
       <button onClick={handleClick}>Click me </button>
-      <button
-        onClick={() => {
-          handleClickAgain("Mario");
-        }}
-      >
-        Click me Again
-      </button>
+      {/* <button onClick={() => handleClickAgain("Mario")}>Click me Again</button> */}
     </div>
   );
 };
