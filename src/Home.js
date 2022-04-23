d0563e66b2a38965ea123bad3345cd1c9c08b95b
Tony Maluki <tonymaluki@gmail.com>
2022-04-23 13:26:25 +0300

    pulling the strings

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,29 @@
 import { useState } from "react";
+
 const Home = () => {
-  //let name = "Tony";
-  const [name, setName] = useState("Tony");
-  const [age, setAge] = useState(25);
-  const handleClick = () => {
-    setName("Maluki");
-    setAge("30");
-  };
-  const handleClickAgain = (name) => {
-    alert("Hello " + name);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "Tony", id: 1 },
+    {
+      title: "Welcome party!",
+      body: "lorem ipsum...",
+      author: "Maluki",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click me </button>
-      {/* <button onClick={() => handleClickAgain("Mario")}>Click me Again</button> */}
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
